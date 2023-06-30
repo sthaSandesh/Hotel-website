6bec6def63dbce954687415b0a9c99c4caa5edfb
sthaSandesh <126145622+sthaSandesh@users.noreply.github.com>
2023-06-30 07:00:01 +0545

    button-link

--- src/component/Hero/hero.jsx
+++ src/component/Hero/hero.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'raviger'
 import React from 'react'
 
 
@@ -8,7 +9,7 @@ export function Hero() {
         <div className="hero-content text-center text-neutral-content">
           <div>
             <h1 className="mb-5 text-5xl font-bold text-white">A Memorable Expreience.</h1>
-            <button className="btn btn-primary bg-purple-500 hover:bg-purple-500">Reserve Now</button>
+            <Link href='/rooms-and-suites'><button className="btn btn-primary bg-purple-500 hover:bg-purple-500">Reserve Now</button></Link>
           </div>
         </div>
       </div>
